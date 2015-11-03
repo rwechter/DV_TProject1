a80be7e0380a276122f485775e4175061839ed6a
Varun Parameswaran <vparam15@gmail.com>
2015-11-02 22:08:12 -0600

    Started rstudio stuff

---
+++ 01 Data/Project3_ETL_Data2.R
@@ -0,0 +1,4 @@
+COD <- data.frame(fromJSON(getURL(URLencode('sayonara.microlab.cs.utexas.edu:5001/rest/native/?query="select * from NHCE"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='C##cs329e_riw223', PASS='orcl_riw223', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+summary (COD)
+head(COD)
